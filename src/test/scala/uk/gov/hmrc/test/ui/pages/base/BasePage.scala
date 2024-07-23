9a48701b57981a00d30827cc967b1195d9ff83aa
Tim <glidester@gmail.com>
2024-07-23 12:12:31 +0100

    BAU update service name

--- src/test/scala/uk/gov/hmrc/test/ui/pages/base/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/base/BasePage.scala
@@ -63,7 +63,7 @@ trait BasePage extends CacheHelper with DriverHelper with PageHelper with LazyLo
       if (elementByIdDoesNotExist("section-header")) ""
       else s" - ${findElementById("section-header").getText}"
 
-    driver.getTitle mustBe title + sectionHeader + " - Make an export declaration online - GOV.UK"
+    driver.getTitle mustBe title + sectionHeader + " - Make and manage an export declaration online - GOV.UK"
     findElementsByTag("h1").head.getText mustBe title
   }
 
