f9430b4dafb3498afcc93f4b3d4498be8fccc46a
Neetha Shivanna <83287552+Neetha-sudo@users.noreply.github.com>
2025-10-17 16:41:28 +0100

    DCWL-3839 ui-test-runner upgrade (#82)
    * DCWL-3839 ui-test-runner upgrade
    
    * DCWL-3839 ui-test-runner upgrade
    
    * DCWL-3839 ui-test-runner upgrade
    
    * DCWL-3839 ui-test-runner upgrade
    
    * DCWL-3839 ui-test-runner upgrade

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,17 +3,15 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "com.typesafe"          % "config"          % "1.4.3"     % Test,
+    "com.typesafe"          % "config"          % "1.4.5"     % Test,
     "com.vladsch.flexmark"  % "flexmark-all"    % "0.64.8"    % Test,
-    "org.scalatest"         %% "scalatest"      % "3.2.18"    % Test,
+    "org.scalatest"         %% "scalatest"      % "3.2.19"    % Test,
     "org.scalatestplus"     %% "selenium-4-12"  % "3.2.17.0"  % Test,
-    "io.cucumber"           %% "cucumber-scala" % "8.21.0"    % Test,
+    "io.cucumber"           %% "cucumber-scala" % "8.23.0"    % Test,
     "io.cucumber"           % "cucumber-junit"  % "7.18.0"    % Test,
     "junit"                 % "junit"           % "4.13.2"    % Test,
     "com.novocode"          % "junit-interface" % "0.11"      % Test,
-    "uk.gov.hmrc"           %% "ui-test-runner" % "0.45.0"    % Test,
-    "io.cucumber"           %% "cucumber-scala" % "8.21.0"    % Test,
-    "io.cucumber"           % "cucumber-junit"  % "7.18.0"    % Test,
+    "uk.gov.hmrc"           %% "ui-test-runner" % "0.50.0"    % Test,
     "junit"                 % "junit"           % "4.13.2"    % Test,
     "com.novocode"          % "junit-interface" % "0.11"      % Test,
     "org.slf4j"             % "slf4j-simple"    % "2.0.13"    % Test,
