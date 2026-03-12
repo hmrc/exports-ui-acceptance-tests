b4084f6f50d373560a4fe95b6d74f9c86daeb074
tomndennis <tomndennis@users.noreply.github.com>
2026-03-12 14:04:09 +0000

    DCWL-4550 - Dependency update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     "io.cucumber"           % "cucumber-junit"  % "7.18.0"    % Test,
     "junit"                 % "junit"           % "4.13.2"    % Test,
     "com.novocode"          % "junit-interface" % "0.11"      % Test,
-    "uk.gov.hmrc"           %% "ui-test-runner" % "0.52.0"    % Test,
+    "uk.gov.hmrc"           %% "ui-test-runner" % "0.53.0"    % Test,
     "junit"                 % "junit"           % "4.13.2"    % Test,
     "com.novocode"          % "junit-interface" % "0.11"      % Test,
     "org.slf4j"             % "slf4j-simple"    % "2.0.17"    % Test,
