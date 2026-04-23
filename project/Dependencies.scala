c14d3943bf770b39022f31d645d6d7ca74c6f9b6
Neetha-sudo <83287552+Neetha-sudo@users.noreply.github.com>
2026-04-23 18:55:57 +0100

    DCWL-4180 uk.gov.hmrc:ui-test-runner updated

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "org.scalatestplus"     %% "selenium-4-12"  % "3.2.17.0"  % Test,
     "junit"                 % "junit"           % "4.13.2"    % Test,
     "com.novocode"          % "junit-interface" % "0.11"      % Test,
-    "uk.gov.hmrc"           %% "ui-test-runner" % "0.53.0"    % Test,
+    "uk.gov.hmrc"           %% "ui-test-runner" % "0.54.0"    % Test,
     "com.novocode"          % "junit-interface" % "0.11"      % Test,
     "org.slf4j"             % "slf4j-simple"    % "2.0.17"    % Test
   )
